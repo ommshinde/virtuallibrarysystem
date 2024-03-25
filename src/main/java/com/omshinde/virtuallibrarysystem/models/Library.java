461743a667d95c3ced8390f7c6ee053e1c534a1b
ommshinde <omshinde5143@gmail.com>
2024-03-25 15:01:50 +0530

    books searching functionality added

--- src/main/java/com/omshinde/virtuallibrarysystem/models/Library.java
+++ src/main/java/com/omshinde/virtuallibrarysystem/models/Library.java
@@ -1,6 +1,11 @@
 package com.omshinde.virtuallibrarysystem.models;
 import com.omshinde.virtuallibrarysystem.models.Book;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
 public class Library {
     HashMap<String, Book> bookList=new HashMap<>();
     private int addedBook=0, skippedBook=0;
@@ -21,4 +26,29 @@ public class Library {
     public String status(){
         return "No of Books added : "+addedBook+" & no of Books skipped : "+skippedBook;
     }
+
+    public List<Book> searchBook(String search){
+        List<Book> booksFound=new ArrayList<>();
+        String lowerCaseSearch=search.toLowerCase();
+//        LocalDate searchDate= LocalDate.parse(search);
+
+        for(Book book:bookList.values()){
+            String title=book.getTitle().toLowerCase();
+            String author=book.getAuthor().toLowerCase();
+            String ISBN=book.getISBN().toLowerCase();
+            String genre= book.getGenre().toLowerCase();
+            String copies= String.valueOf(book.getNoOfCopies());
+//            LocalDate publicationDate=book.getPublicationDate();
+
+            if(title.contains(search.toLowerCase())||author.contains(search.toLowerCase())||ISBN.contains(search.toLowerCase())||genre.contains(search.toLowerCase())||copies.contains(search)){
+                booksFound.add(book);
+            }
+        }
+
+        if (booksFound.isEmpty()){
+            System.out.println("No books found.");
+        }
+        return booksFound;
+    }
+
 }
