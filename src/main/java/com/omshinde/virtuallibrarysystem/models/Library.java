8ea623a730235f10b5ea36a5feb461c3936e50d9
ommshinde <omshinde5143@gmail.com>
2024-03-25 10:31:16 +0530

    added new class library containing all books

--- src/main/java/com/omshinde/virtuallibrarysystem/models/Library.java
+++ src/main/java/com/omshinde/virtuallibrarysystem/models/Library.java
@@ -2,17 +2,17 @@ package com.omshinde.virtuallibrarysystem.models;
 import com.omshinde.virtuallibrarysystem.models.Book;
 import java.util.HashMap;
 public class Library {
-    HashMap<String, Book> booklist=new HashMap<>();
+    HashMap<String, Book> bookList=new HashMap<>();
 
     public boolean addBook(Book book){
-        if(!booklist.containsKey(book.getISBN())){
-            booklist.put(book.getISBN(), book);
+        if(!bookList.containsKey(book.getISBN())){
+            bookList.put(book.getISBN(), book);
             return true;
         }
         return false;
     }
 
     public boolean isISBNUnique(String ISBN){
-        return !booklist.containsKey(ISBN);
+        return !bookList.containsKey(ISBN);
     }
 }
