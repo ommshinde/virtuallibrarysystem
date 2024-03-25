3dbf2a286ff66ff414de5b207f99dd28f96ce52b
ommshinde <omshinde5143@gmail.com>
2024-03-25 10:51:44 +0530

    added status of library

--- src/main/java/com/omshinde/virtuallibrarysystem/models/Library.java
+++ src/main/java/com/omshinde/virtuallibrarysystem/models/Library.java
@@ -3,16 +3,22 @@ import com.omshinde.virtuallibrarysystem.models.Book;
 import java.util.HashMap;
 public class Library {
     HashMap<String, Book> bookList=new HashMap<>();
+    private int addedBook=0, skippedBook=0;
 
     public boolean addBook(Book book){
         if(!bookList.containsKey(book.getISBN())){
             bookList.put(book.getISBN(), book);
+            addedBook++;
             return true;
         }
+        skippedBook++;
         return false;
     }
-
     public boolean isISBNUnique(String ISBN){
         return !bookList.containsKey(ISBN);
+    }
+
+    public String status(){
+        return "No of Books added : "+addedBook+" & no of Books skipped : "+skippedBook;
     }
 }
