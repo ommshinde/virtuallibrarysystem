cda2a8d2c01b492b210dccabdc56bdfb53a3f8fc
ommshinde <omshinde5143@gmail.com>
2024-03-25 09:50:58 +0530

    set the book class with constructor and getters and setters

--- src/main/java/com/omshinde/virtuallibrarysystem/models/Book.java
+++ src/main/java/com/omshinde/virtuallibrarysystem/models/Book.java
@@ -1,12 +1,66 @@
 package com.omshinde.virtuallibrarysystem.models;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Book {
     private String title, author, ISBN, genre;
-    private Date publication_date;
-
+    private LocalDate publicationDate;
     private int noOfCopies;
 
+    public Book(String title, String author, String ISBN, String genre, LocalDate publicationDate, int noOfCopies){
+        this.title=title;
+        this.author=author;
+        this.ISBN=ISBN;
+        this.genre=genre;
+        this.publicationDate=publicationDate;
+        this.noOfCopies=noOfCopies;
+    }
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(){
+        this.title=title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getpublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public int getNoOfCopies() {
+        return noOfCopies;
+    }
+
+    public void setNoOfCopies(int noOfCopies) {
+        this.noOfCopies = noOfCopies;
+    }
 
 }
