4ba573dc3b348c8bfb231a12f167f02bfe05c37a
ommshinde <omshinde5143@gmail.com>
2024-03-25 09:39:12 +0530

    added all variable

--- src/main/java/com/omshinde/virtuallibrarysystem/models/Book.java
+++ src/main/java/com/omshinde/virtuallibrarysystem/models/Book.java
@@ -1,6 +1,12 @@
 package com.omshinde.virtuallibrarysystem.models;
 
+import java.util.Date;
+
 public class Book {
-    private String title, author;
+    private String title, author, ISBN, genre;
+    private Date publication_date;
+
+    private int noOfCopies;
+
 
 }
